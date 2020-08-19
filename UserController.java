090d29cf0d7d9db102ebcb37fc606ab1b468398e
wjflwkd123 <wjflwkd123@gmail.com>
2020-08-19 12:55:14 +0900

    Add UserController [Delete & coment]

--- UserController.java
+++ UserController.java
@@ -23,5 +23,13 @@ public class UserController {
         logger.info("경로 : update");
 
         return "user/update";
-    }	
+    }
+
+    // 회원삭제 페이지로 이동
+    @RequestMapping("/user/delete.do) 
+    public String delete() {
+        logger.info("경로 : delete");
+
+        return "user/delete";
+    }		
 }
